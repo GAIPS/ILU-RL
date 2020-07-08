6a2d4918422ba1c429b718a264143e17a8e4de8b
gsavarela <guilhermevarela@protonmail.com>
2020-07-08 08:53:18 +0100

    Add time feature as global feature for state  (#55)

--- ilurl/state/state.py
+++ ilurl/state/state.py
@@ -1,7 +1,18 @@
+import numpy as np
+
 from ilurl.state.elements import Intersection
 from ilurl.utils.aux import flatten as flat
 
+
 class State:
+    """Describes the state space
+
+        * Forest: composed of root nodes of trees (intersections).
+        * Receives kernel data from vehicles (e.g speed) and intersections (e.g states).
+        * Broadcasts data to intersections.
+        * Outputs features.
+        * Computes global features e.g Time
+    """
 
     def __init__(self, network, mdp_params):
         """
@@ -18,9 +29,17 @@ class State:
 
         * state
         """
-        # 
         self._tls_ids = network.tls_ids
         # Global features: e.g Time
+        self._time = -1
+        self._has_time = False
+        if 'time' in mdp_params.states and mdp_params.time_period is not None:
+            self._has_time = True
+            self._bins = mdp_params.category_times
+            self._last_time = -1
+            self._time_period = mdp_params.time_period
+
+
         # Local features.
         self._intersections = []
 
@@ -31,15 +50,20 @@ class State:
                                  network.tls_phases[tls_id],
                                  network.tls_max_capacity[tls_id])
             for tls_id in network.tls_ids}
-            # TODO: replace mdp_params.states --> features
 
     @property
     def tls_ids(self):
         return self._tls_ids
 
+    @property
+    def time(self):
+        return self._time
 
     def update(self, duration, vehs, tls=None):
+        # 1) Update time.
+        self._update_time(duration)
 
+        # 2) Broadcast update to intersections.
         for tls_id, i in self._intersections.items():
             i.update(duration, vehs[tls_id], tls)
 
@@ -52,3 +76,31 @@ class State:
         if flatten:
             ret = {k: tuple(flat(v)) for k, v in ret.items()}
         return ret
+
+    def _update_time(self, duration):
+        """Update time as feature"""
+        if self._has_time and self._last_time != duration:
+            self._time += 1
+            self._last_time = duration
+
+    def _add_time(self, filter_by, categorize, split, features):
+        """Add time as feature to intersections"""
+        if self._has_time:
+            period = self._time_period
+            # 1) Verify time conditions.
+            if filter_by is None or ('time' in filter_by):
+                # 2) Number of periods (e.g hours) % Rolling update.
+                ret  = (self.time // period) % int(24 * 3600 / period)
+
+                # 3) Convert into category.
+                if categorize:
+                    ret = np.digitize(time, self._bins)
+
+                # 4) Add to features.
+                if split:
+                    ret = tuple([ret] + list(features))
+                else:
+                    ret = [ret] + list(features)
+                return ret
+            return features
+        return features
