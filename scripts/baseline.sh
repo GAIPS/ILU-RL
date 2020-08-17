ac150a589f06f403b4be86dfdfd9b1c51aa1327a
gsavarela <guilhermevarela@protonmail.com>
2020-08-17 12:37:41 +0100

    Update script/baseline.sh variable train.config

--- scripts/baseline.sh
+++ scripts/baseline.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-python jobs/baseline.py $1
+sed -i 's/network = grid/network \= grid_6/'  config/train.config
+python jobs/baseline.py max_pressure
+
+sed -i 's/network = grid_6/network \= grid_12/'  config/train.config
+python jobs/baseline.py max_pressure
+
+sed -i 's/network = grid_12/network \= grid_21/'  config/train.config
+python jobs/baseline.py max_pressure
