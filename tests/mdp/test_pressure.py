c071cdf63599cefbc116e132305d773698b778d4
gsavarela <guilhermevarela@protonmail.com>
2020-09-03 15:07:11 +0100

    Decouple network from state & reward tests

--- tests/mdp/test_pressure.py
+++ tests/mdp/test_pressure.py
@@ -1,51 +1,15 @@
-import pickle
 import unittest
-
-import numpy as np
 
 from ilurl.state import State
 from ilurl.envs.elements import build_vehicles
 from ilurl.rewards import build_rewards
 from ilurl.params import MDPParams
-from ilurl.networks.base import Network
 from ilurl.utils.aux import flatten
 from ilurl.utils.properties import lazy_property
 
-# incoming approaches
-INCOMING_247123161 = {0: [('309265401', 0), ('309265401', 1), ('-238059328', 0), ('-238059328', 1)],
-                      1: [('383432312', 0), ('-238059324', 0)]}
+from tests.network.test_grid import *
 
-INCOMING_247123464 = {0: [('309265400', 0), ('309265400', 1), ('-309265401', 0), ('-309265401', 1)],
-    1: [('22941893', 0)]}
-INCOMING_247123468 = {0: [('309265402', 0), ('309265402', 1), ('-309265400', 0), ('-309265400', 1)], 1: [('23148196', 0)]}
-
-# internal outgoing approaches; lanes between two tl
-INT_OUTGOING_247123161 = [('-309265401', 0), ('-309265401', 1)]
-INT_OUTGOING_247123464 = [('-309265400', 0), ('-309265400', 1), ('309265401', 0), ('309265401', 1)]
-INT_OUTGOING_247123468 = [('309265400', 0), ('309265400', 1)]
-
-class TestBase(unittest.TestCase):
-    """
-        * Defines a network and the kernel data.
-
-        * Common base for problem formulation tests.
-    """
-    def setUp(self):
-
-        network_args = {
-            'network_id': 'grid',
-            'horizon': 999,
-            'demand_type': 'constant',
-            'tls_type': 'rl'
-        }
-        self.network = Network(**network_args)
-
-        with open('tests/data/grid_kernel_data.dat', "rb") as f:
-            kernel_data = pickle.load(f)
-        self.kernel_data = kernel_data
-
-
-class TestPressure(TestBase):
+class TestPressure(TestGridBase):
     """
         * Tests pressure related state and reward
 
