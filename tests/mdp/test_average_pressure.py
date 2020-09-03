e47f647fb24f60c3fb261f7a38655090c8a64ff9
gsavarela <guilhermevarela@protonmail.com>
2020-09-03 17:27:28 +0100

    Extend unittest state and reward avg.pressure

--- tests/mdp/test_average_pressure.py
+++ tests/mdp/test_average_pressure.py
@@ -128,7 +128,7 @@ class TestAveragePressure(TestGridBase):
     #     self.assertEqual(test, sol)
 
     def test_avg_pressure_tl1ph0(self):
-        """Tests pressure state
+        """Tests average pressure state
             * traffic light 1
             * ID = '247123161'
             * phase 0
@@ -143,7 +143,7 @@ class TestAveragePressure(TestGridBase):
         # State.
         # 247123161 static assertion
         # avg.pressure, phase 0
-        self.assertEqual(self.state[ID][0], 3.73, f'avg.pressure:{ID}\tphase:0') 
+        self.assertEqual(self.state[ID][0], 3.73, f'avg.pressure:{ID}\tphase:0')
 
         # 247123161 dynamic assertion
         # avg.pressure, phase 0
@@ -181,7 +181,7 @@ class TestAveragePressure(TestGridBase):
         # ipdb.set_trace()
     #     return round(press / 60, 2)
     def test_avg_pressure_tl1ph1(self):
-        """Tests pressure state
+        """Tests average pressure state
             * traffic light 1
             * ID = '247123161'
             * phase 1
@@ -204,7 +204,7 @@ class TestAveragePressure(TestGridBase):
 
 
     def test_min_avg_pressure_tl1(self):
-        """Tests pressure reward
+        """Tests average pressure reward
             * traffic light 1
             * ID = '247123161'
         """
@@ -212,108 +212,104 @@ class TestAveragePressure(TestGridBase):
         reward = self.reward(self.observation_space)
         self.assertAlmostEqual(reward[ID], -0.01*(3.73 + 1.88))
 
-    # def test_pressure_tl2ph0(self):
-    #     """Tests pressure state
-    #         * traffic light 2
-    #         * ID = '247123464'
-    #         * phase 0
-    #     """
-    #     ID = '247123464'
+    def test_avg_pressure_tl2ph0(self):
+        """Tests average pressure state
+            * traffic light 2
+            * ID = '247123464'
+            * phase 0
+        """
+        ID = '247123464'
 
-    #     outgoing = INT_OUTGOING_247123464
-    #     incoming = INCOMING_247123464[0]
+        outgoing = INT_OUTGOING_247123464
+        incoming = INCOMING_247123464[0]
 
-    #     p0  = process_pressure(self.kernel_data, incoming, outgoing)
+        p0  = process_pressure(self.kernel_data, incoming, outgoing)
 
-    #     # State.
-    #     # 247123464 static assertion
-    #     self.assertEqual(self.state[ID][0], -3.0) # pressure, phase 0
+        # State.
+        # 247123464 static assertion
+        self.assertEqual(self.state[ID][0], -2.58) # pressure, phase 0
 
-    #     # 247123464 dynamic assertion
-    #     self.assertEqual(self.state[ID][0], p0) # pressure, phase 0
+        # 247123464 dynamic assertion
+        self.assertEqual(self.state[ID][0], p0) # pressure, phase 0
 
-    # def test_pressure_tl2ph1(self):
-    #     """Tests pressure state
-    #         * traffic light 2
-    #         * ID = '247123464'
-    #         * phase 1
-    #     """
-    #     ID = '247123464'
+    def test_avg_pressure_tl2ph1(self):
+        """Tests average pressure state
+            * traffic light 2
+            * ID = '247123464'
+            * phase 1
+        """
+        ID = '247123464'
 
-    #     outgoing = INT_OUTGOING_247123464
-    #     incoming = INCOMING_247123464[1]
+        outgoing = INT_OUTGOING_247123464
+        incoming = INCOMING_247123464[1]
 
-    #     p1  = process_pressure(self.kernel_data, incoming, outgoing)
+        p1  = process_pressure(self.kernel_data, incoming, outgoing)
 
-    #     # State.
-    #     # 247123464 static assertion
-    #     self.assertEqual(self.state[ID][1], -2.0) # pressure, phase 1
+        # State.
+        # 247123464 static assertion
+        self.assertEqual(self.state[ID][1], -2.95) # pressure, phase 1
 
-    #     # 247123464 dynamic assertion
-    #     self.assertEqual(self.state[ID][1], p1) # pressure, phase 1
+        # 247123464 dynamic assertion
+        self.assertEqual(self.state[ID][1], p1) # pressure, phase 1
 
-    #     # # Reward.
-    #     # reward = self.reward(self.observation_space)
-    #     # self.assertEqual(reward[ID], 0.01*(3.0 + 2.0))
+    def test_min_avg_pressure_tl2(self):
+        """Tests avg pressure reward
+            * traffic light 2
+            * ID = '247123464'
+        """
+        ID = '247123464'
+        reward = self.reward(self.observation_space)
+        self.assertEqual(reward[ID], 0.01*(2.58 + 2.95))
 
-    # def test_min_pressure_tl2(self):
-    #     """Tests pressure reward
-    #         * traffic light 2
-    #         * ID = '247123464'
-    #     """
-    #     ID = '247123464'
-    #     reward = self.reward(self.observation_space)
-    #     self.assertEqual(reward[ID], 0.01*(3.0 + 2.0))
+    def test_avg_pressure_tl3ph0(self):
+        """Tests avg pressure state
+            * traffic light 3
+            * ID = '247123468'
+            * phase 0
+        """
+        ID = '247123468'
 
-    # def test_pressure_tl3ph0(self):
-    #     """Tests pressure state
-    #         * traffic light 3
-    #         * ID = '247123468'
-    #         * phase 0
-    #     """
-    #     ID = '247123468'
+        outgoing = INT_OUTGOING_247123468
+        incoming = INCOMING_247123468[0]
 
-    #     outgoing = INT_OUTGOING_247123468
-    #     incoming = INCOMING_247123468[0]
+        p0 = process_pressure(self.kernel_data, incoming, outgoing)
 
-    #     p0 = process_pressure(self.kernel_data, incoming, outgoing)
+        # State.
+        # 247123468 static assertion
+        self.assertEqual(self.state[ID][0], 0.73) # pressure, phase 0
 
-    #     # State.
-    #     # 247123468 static assertion
-    #     self.assertEqual(self.state[ID][0], 1.0) # pressure, phase 0
-
-    #     # 247123468 dynamic assertion
-    #     self.assertEqual(self.state[ID][0], p0) # pressure, phase 0
+        # 247123468 dynamic assertion
+        self.assertEqual(self.state[ID][0], p0) # pressure, phase 0
 
 
-    # def test_pressure_tl3ph1(self):
-    #     """Tests pressure state
-    #         * traffic light 3
-    #         * ID = '247123468'
-    #         * phase 1
-    #     """
-    #     ID = '247123468'
+    def test_avg_pressure_tl3ph1(self):
+        """Tests avg pressure state
+            * traffic light 3
+            * ID = '247123468'
+            * phase 1
+        """
+        ID = '247123468'
 
-    #     outgoing = INT_OUTGOING_247123468
-    #     incoming = INCOMING_247123468[1]
+        outgoing = INT_OUTGOING_247123468
+        incoming = INCOMING_247123468[1]
 
-    #     p1 = process_pressure(self.kernel_data, incoming, outgoing)
+        p1 = process_pressure(self.kernel_data, incoming, outgoing)
 
-    #     # State.
-    #     # 247123468 static assertion
-    #     self.assertEqual(self.state[ID][1], 0.0) # pressure, phase 1
+        # State.
+        # 247123468 static assertion
+        self.assertEqual(self.state[ID][1], 0.02) # pressure, phase 1
 
-    #     # 247123468 dynamic assertion
-    #     self.assertEqual(self.state[ID][1], p1) # pressure, phase 1
+        # 247123468 dynamic assertion
+        self.assertEqual(self.state[ID][1], p1) # pressure, phase 1
 
-    # def test_min_pressure_tl3(self):
-    #     """Tests pressure reward
-    #         * traffic light 3
-    #         * ID = '247123468'
-    #     """
-    #     ID = '247123468'
-    #     reward = self.reward(self.observation_space)
-    #     self.assertEqual(reward[ID], -0.01*(1.0 + 0.0))
+    def test_min_pressure_tl3(self):
+        """Tests pressure reward
+            * traffic light 3
+            * ID = '247123468'
+        """
+        ID = '247123468'
+        reward = self.reward(self.observation_space)
+        self.assertEqual(reward[ID], -0.01*(0.73 + 0.02))
 
 def process_pressure(kernel_data, incoming, outgoing):
 
