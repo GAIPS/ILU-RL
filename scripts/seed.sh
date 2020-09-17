274cc9cbc710275ce9d40660e7e846b2b524fcb8
Varela <guilhermevarela@protonmail.com>
2020-09-17 15:02:05 +0100

    Set config INTER., QL, 50k, 60s, 30*3, WAIT

---
+++ scripts/seed.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# python jobs/train.py | xargs python jobs/rollouts.py | xargs python jobs/test.py | xargs python analysis/rollouts.py
+# add 100
+python jobs/run.py
+
+sed -i 's/\[0, 10, 20, 30, 40\]/\[50, 60, 70, 80, 90\]/g' config/run.config
+python jobs/run.py
+
+
+sed -i 's/\[50, 60, 70, 80, 90\]/\[100, 110, 120, 130, 140\]/g' config/run.config
+python jobs/run.py
+
+sed -i 's/\[100, 110, 120, 130, 140\]/\[150, 160, 170, 180, 190\]/g' config/run.config
+python jobs/run.py
+
+sed -i 's/\[150, 160, 170, 180, 190\]/\[200, 210, 220, 230, 240\]/g' config/run.config
+python jobs/run.py
