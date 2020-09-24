86ae4f90773777ab1f86a1439aa456f4434ec5a5
Varela <guilhermevarela@protonmail.com>
2020-09-24 16:48:38 +0100

    Update state vs state scatter plot

--- analysis/scatter.py
+++ analysis/scatter.py
@@ -1,38 +1,36 @@
-"""This script makes a scatter plot from observation spaces
+"""This script makes a scatter plot state vs state
 
-    Use this script to determine a discretization scheme
+    TODO:
+    -----
+    1) Segregate phases: 1 axis => two phases.
+    4) Relax speed and count to any features.
+    5) Consolidate tls signals
+    2) Label category digits
+    3) Reward vs state space observations
+    
 
     USAGE:
     -----
-    From root directory with files saved on root
-    > python analysis/scatter.py
+
+    From directory
+    > python analysis/scatter.py 20200923143127.894361/intersection_20200923-1431271600867887.8995893/
 
 """
-__author__ = 'Guilherme Varela'
-__date__ = '2020-02-22'
 # core packages
+import re
+from pathlib import Path
 from collections import defaultdict
 import json
-import os
+from os import environ
 from glob import glob
 import argparse
 
 # third-party libs
-import dill
-import numpy as np
-from scipy.stats import norm
+import configparser
 import matplotlib.pyplot as plt
 
 # project dependencies
-from flow.core.params import EnvParams
- 
-# ilurl dependencies
-from ilurl.params import QLParams
-from ilurl.utils import TIMESTAMP_PROG
-
-
-ROOT_DIR = os.environ['ILURL_HOME']
-
+from ilurl.utils.aux import TIMESTAMP
 
 def get_arguments():
     parser = argparse.ArgumentParser(
@@ -43,11 +41,6 @@ def get_arguments():
     )
     parser.add_argument('experiment_dir', type=str, nargs='?',
                         help='Directory to the experiment')
-
-    parser.add_argument('--evaluation', '-e', dest='eval_db',
-                        type=str2bool, nargs='?', default=True,
-                        help='''Either `train` or `evaluation` 
-                            defaults to evaluation''')
 
     return parser.parse_args()
 
@@ -65,52 +58,30 @@ def str2bool(v):
 if __name__ == '__main__':
     # this loop acumulates experiments
     args = get_arguments()
-    exp_dir = args.experiment_dir
-    eval_db = args.eval_db
-
-    if eval_db:
-        ext = '.l.eval.info.json'
-    else:
-        ext = 'train.json'
+    experiment_path = Path(args.experiment_dir)
+    train_log_path = experiment_path / 'logs' / 'train_log.json'
+    config_path = experiment_path / 'config' / 'train.config'
 
     phases = defaultdict(list)
-    labels = []
-    category_speeds = None
-    category_counts = None
-    output_json = glob(f'{exp_dir}*{ext}')[0]
-    params_json = glob(f'{exp_dir}*params.json')[0]
-
-    with open(output_json, 'r') as f:
+    with train_log_path.open('r') as f:
         output = json.load(f)
     observation_spaces = output['observation_spaces']
 
-    # on evaluation observation states are bound to
-    # the iteration Q was saved.
-    if eval_db and isinstance(observation_spaces, dict):
-        key = max([int(k) for k in observation_spaces])
-        observation_spaces = observation_spaces[str(key)]
-    elif not eval_db:
-        observation_spaces = [observation_spaces]
+    train_config = configparser.ConfigParser()
+    train_config.read(config_path.as_posix())
+    # TODO: Use the features to get the categories
+    labels = eval(train_config.get('mdp_args', 'features'))
+    network_id = train_config.get('train_args', 'network')
 
 
-    with open(params_json, 'r') as f:
-        params = json.load(f)
-    ql_params = QLParams(**params['ql_args'])
-    env_params = EnvParams(**params['env_args'])
 
-    labels = ql_params.states_labels
-
-    additional_params = env_params.additional_params
-
-    category_speeds = ql_params.category_speeds
-
-    category_counts = ql_params.category_counts
-
+    category_speeds = eval(train_config.get('mdp_args', 'category_speeds'))
+    category_counts = eval(train_config.get('mdp_args', 'category_counts'))
     for observation_space in observation_spaces:
-        for intersection_space in observation_space:
-            for phase_space in intersection_space:
-                for i, phase in enumerate(phase_space):
-                    phases[i] += [phase]
+        # TODO: Relax constraints for multiple tls
+        for intersection_space in observation_space.values():
+            for num, phase_space in enumerate(intersection_space):
+                phases[num].append(phase_space)
 
     _, ax = plt.subplots()
     for i, label in enumerate(labels):
@@ -136,15 +107,15 @@ if __name__ == '__main__':
         N += len(x)
         ax.scatter(x, y, c=colors[i], label=f'phase#{i}')
 
-    filename = os.path.basename(output_json)
-    filename = filename.replace(ext, '')
+    expid = experiment_path.parts[-1]
+    result = re.search(TIMESTAMP, expid)
 
-    result = TIMESTAMP_PROG.search(filename)
     if result:
-        timestamp = f'_{result.group(0,)}'
-        filename = filename.replace(timestamp, '')
+        timestamp = result.group(0,)
+    else:
+        timetamp = expid
     ax.legend()
     ax.grid(True)
-    plt.title(f'{filename}:\nobservation space (N={N})')
-    plt.savefig(f'{exp_dir}/scatter.png')
+    plt.title(f'{network_id}\n{timestamp}:\nobservation space (N={N})')
+    plt.savefig(experiment_path / 'scatter.png')
     plt.show()
