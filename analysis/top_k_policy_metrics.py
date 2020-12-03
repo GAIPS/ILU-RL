daf3ccb0cb2775bcd58cb04f00c1e8c6cf07da0f
PPSantos <pedro.pinto.santos@tecnico.ulisboa.pt>
2020-12-03 23:35:02 +0000

    Fixed bug in top_k_policy_metrics script when used with baselines.

--- analysis/top_k_policy_metrics.py
+++ analysis/top_k_policy_metrics.py
@@ -7,6 +7,8 @@ import numpy as np
 from pathlib import Path
 from scipy import stats
 import configparser
+import tempfile
+import shutil
 
 
 def get_arguments():
@@ -35,15 +37,45 @@ def main():
     args = get_arguments()
     print_arguments(args)
 
+    if Path(args.experiment_root_folder).suffix != '.gz':
+        # Uncompressed file (experiment_folder).
+        raise ValueError('Not implemented for uncompressed folders. Please point to a file with .tar.gz extension.')
+
+    tar = tarfile.open(args.experiment_root_folder)
+    all_names = tar.getnames()
+
+    # Get one of the config files.
+    config_files = [x for x in all_names if Path(x).name == 'train.config']
+    config_p = config_files[0]
+
+    # Create temporary directory.
+    dirpath = tempfile.mkdtemp()
+
+    # Extract config file to temporary directory.
+    tar.extract(config_p, dirpath)
+
+    train_config = configparser.ConfigParser()
+    train_config.read(dirpath + '/' + config_p)
+
+    # Print config file.
+    tls_type = train_config['train_args']['tls_type']
+
+    print(f'tls_type: {tls_type}\n')
+
+    # Clean temporary directory.
+    shutil.rmtree(dirpath)
+
     exp_name = os.path.basename(args.experiment_root_folder)
     exp_name = exp_name.split('.')[0] + '.' + exp_name.split('.')[1]
 
     df_metrics = pd.read_csv(tarfile.open(args.experiment_root_folder).extractfile(
                             f'{exp_name}/plots/test/{exp_name}_metrics.csv'))
 
-    df_metrics_grouped = df_metrics.groupby(['train_run']).mean()
 
-    top_k = df_metrics_grouped.nsmallest(args.num_samples, 'travel_time')
+    if tls_type == 'rl':
+        df_metrics = df_metrics.groupby(['train_run']).mean()
+
+    top_k = df_metrics.nsmallest(args.num_samples, 'travel_time')
     top_k_mean = top_k.mean()
 
     print(f'Top-{args.num_samples} policies (w.r.t. travel_time):')
