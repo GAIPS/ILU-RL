d3a049088fa4dcd5915bf2ac2e728ced63bdd125
gsavarela <guilhermevarela@protonmail.com>
2020-12-04 19:47:03 +0000

    Retire deprecated tests && Add time_period_test

--- tests/unit/mdp/test_time_period.py
+++ tests/unit/mdp/test_time_period.py
@@ -1,29 +1,37 @@
-import numpy as np
-
+import pickle
 import unittest
 
+import numpy as np
+
 from ilurl.state import State
+from ilurl.envs.elements import build_vehicles
 from ilurl.params import MDPParams
-from ilurl.utils.properties import lazy_property
+from ilurl.networks.base import Network
+from ilurl.utils.aux import flatten
 
-from tests.unit.mdp.test_mdp_base import TestGridMDPSetUp
 
-class TestGridTimePeriodSyncVersion(TestGridMDPSetUp):
+class TestStateReward(unittest.TestCase):
     """
-        * Tests time period variable wrt Grid network.
-            (time period is synchronous in comparison
-            to the cycle length)
-
-        * Set of tests that target the implemented
-          problem formulations, i.e. state and reward
-          function definitions.
-
-        * Use lazy_properties to compute once and use
-          as many times as you want -- it's a cached
-          property
+        Set of tests that target the implemented
+        problem formulations, i.e. state and reward
+        function definitions.
     """
-    @lazy_property
-    def mdp_params(self):
+
+    def setUp(self):
+
+        network_args = {
+            'network_id': 'grid',
+            'horizon': 999,
+            'demand_type': 'constant',
+            'tls_type': 'rl'
+        }
+        self.network = Network(**network_args)
+
+
+    def test_time_period(self):
+        """
+            Time period.
+        """
         mdp_params = MDPParams(
                         features=('delay',),
                         reward='reward_min_delay',
@@ -32,73 +40,39 @@ class TestGridTimePeriodSyncVersion(TestGridMDPSetUp):
                         reward_rescale=0.01,
                         time_period=3600,
                         velocity_threshold=0.1)
-        return mdp_params
 
-    def setUp(self):
-        """Code here will run before every test"""
-        super(TestGridTimePeriodSyncVersion, self).setUp()
+        self.observation_space = State(self.network, mdp_params)
+        self.observation_space.reset()
 
-    def test_init_state_tl1_ph0(self):
-        node_id ='247123161'
-        phase_id = 0
+        with open('tests/unit/data/grid_kernel_data.dat', "rb") as f:
+            kernel_data = pickle.load(f)
 
-        check = self.state[node_id][phase_id+1]
-        sol = process_delay(self.kernel_data_1, node_id, phase_id)
+        self.assertEqual(len(kernel_data), 60)
 
-        self.assertEqual(check, 1.35)
-        self.assertEqual(check, sol)
+        # Fake environment interaction with state object.
+        timesteps = list(range(1,60)) + [0]
+        for t, data in zip(timesteps, kernel_data):
+            self.observation_space.update(t, data)
 
-    def test_init_state_tl1_ph1(self):
-        node_id ='247123161'
-        phase_id = 1
+        # Get state.
+        state = self.observation_space.feature_map(
+            categorize=mdp_params.discretize_state_space,
+            flatten=True
+        )
 
-        check = self.state[node_id][phase_id+1]
-        sol = process_delay(self.kernel_data_1, node_id, phase_id)
+        self.assertEqual(len(state['247123161']), 3)
+        self.assertEqual(len(state['247123464']), 3)
+        self.assertEqual(len(state['247123468']), 3)
 
-        self.assertEqual(check, 1.38)
-        self.assertEqual(check, sol)
+        # State.
+        # 247123161.
+        self.assertEqual(state['247123161'][0], 0)    # time variable
 
-    def test_init_state_tl2_ph0(self):
-        node_id ='247123464'
-        phase_id = 0
+        # 247123464.
+        self.assertEqual(state['247123464'][0], 0)    # time variable
 
-        check = self.state[node_id][phase_id+1]
-        sol = process_delay(self.kernel_data_1, node_id, phase_id)
-
-        self.assertEqual(check, 0.01)
-        self.assertEqual(check, sol)
-
-    def test_init_state_tl2_ph1(self):
-        node_id ='247123464'
-        phase_id = 1
-
-        check = self.state[node_id][phase_id+1]
-        sol = process_delay(self.kernel_data_1, node_id, phase_id)
-
-        self.assertEqual(check, 0.62)
-        self.assertEqual(check, sol)
-
-    def test_init_state_tl3_ph0(self):
-        node_id ='247123468'
-        phase_id = 0
-
-        check = self.state[node_id][phase_id+1]
-        sol = process_delay(self.kernel_data_1, node_id, phase_id)
-
-        self.assertEqual(check, 0.85)
-        self.assertEqual(check, sol)
-
-    def test_init_state_tl3_ph1(self):
-        node_id ='247123468'
-        phase_id = 1
-
-        check = self.state[node_id][phase_id+1]
-        sol = process_delay(self.kernel_data_1, node_id, phase_id)
-
-        self.assertEqual(check, 0.9)
-        self.assertEqual(check, sol)
-
-    def test_time_period_sync(self):
+        # 247123468.
+        self.assertEqual(state['247123468'][0], 0)    # time variable
 
         self.observation_space.reset()
 
@@ -109,100 +83,22 @@ class TestGridTimePeriodSyncVersion(TestGridMDPSetUp):
                 # Fake environment interaction with state object.
                 # (60 seconds = 1 minute).
                 timesteps = list(range(1,60)) + [0]
-                for t, data in zip(timesteps, self.kernel_data_1):
+                for t, data in zip(timesteps, kernel_data):
                     self.observation_space.update(t, data)
 
                 # Get state.
                 state = self.observation_space.feature_map(
-                    categorize=self.mdp_params.discretize_state_space,
+                    categorize=mdp_params.discretize_state_space,
                     flatten=True
                 )
 
-                # Assert time period variable.
                 self.assertEqual(state['247123161'][0], hour)
                 self.assertEqual(state['247123464'][0], hour)
                 self.assertEqual(state['247123468'][0], hour)
 
 
-class TestGridTimePeriodAsyncVersion(TestGridMDPSetUp):
-    """
-        * Tests time period variable wrt Grid network.
-            (time period is not synchronous in comparison
-            to the cycle length)
-
-        * Set of tests that target the implemented
-          problem formulations, i.e. state and reward
-          function definitions.
-
-        * Use lazy_properties to compute once and use
-          as many times as you want -- it's a cached
-          property
-    """
-    @lazy_property
-    def mdp_params(self):
-        mdp_params = MDPParams(
-                        features=('delay',),
-                        reward='reward_min_delay',
-                        normalize_velocities=True,
-                        discretize_state_space=False,
-                        reward_rescale=0.01,
-                        time_period=3599,
-                        velocity_threshold=0.1)
-        return mdp_params
-
-    def setUp(self):
-        """Code here will run before every test"""
-        super(TestGridTimePeriodAsyncVersion, self).setUp()
-
-    def test_time_period_async(self):
-
-        self.observation_space.reset()
-
-        hours = list(range(24)) + [0,1]
-        for hour in hours:
-            for minute in range(1,61):
-
-                # Fake environment interaction with state object.
-                # (60 seconds = 1 minute).
-                timesteps = list(range(1,60)) + [0]
-                for t, data in zip(timesteps, self.kernel_data_1):
-                    self.observation_space.update(t, data)
-
-                # Get state.
-                state = self.observation_space.feature_map(
-                    categorize=self.mdp_params.discretize_state_space,
-                    flatten=True
-                )
-
-                seconds_elapsed = hour * (3600) + 60 * (minute)
-                t_period = (seconds_elapsed % 86400) // 3599
-
-                # Assert time period variable.
-                self.assertEqual(state['247123161'][0], t_period)
-                self.assertEqual(state['247123464'][0], t_period)
-                self.assertEqual(state['247123468'][0], t_period)
-
-
-def process_delay(kernel_data, node_id, phase_id):
-    """Processes batched delay computation"""
-    cycle_time = 60
-
-    def delay(x):
-            return np.where(x >= 1, 0.0, np.exp(-5*x))
-
-    values_count = []
-    for t in kernel_data:
-        values_count.extend(t[node_id][phase_id])
-
-    vehs_speeds = []
-    for veh in values_count:
-        vehs_speeds.append(veh.speed)
-
-    vehs_speeds = np.array(vehs_speeds)
-
-    ret = np.sum(delay(vehs_speeds / 13.89)) / cycle_time
-    ret = round(ret, 2)
-    return ret
+    def tearDown(self):
+        pass
 
 
 if __name__ == '__main__':
